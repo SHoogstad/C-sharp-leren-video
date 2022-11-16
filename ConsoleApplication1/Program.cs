b773217a6511e3a6774c6a93bcc8fe5b42b0611d
Sem Hoogstad <shoogstad@icloud.com>
2022-11-16 09:20:08 +0100

    Initial Coomit + methods

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Hello();
+            Console.ReadLine();
+        }
+
+        private static void Hello()
+        {
+            Console.Write("Enter your name:");
+            string name = Console.ReadLine();
+            string message = $"hello {name}!"; 
+            Console.WriteLine(message);
+        }
+    }
+}
